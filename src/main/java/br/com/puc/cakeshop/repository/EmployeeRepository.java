ba0902b84dcea48196cf120e2b41a6ebaa04e189
andersonsantoszup <45801273+andersonsantoszup@users.noreply.github.com>
2020-05-18 13:44:45 -0300

    criação de metodos e regras de negocios

---
+++ src/main/java/br/com/puc/cakeshop/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package br.com.puc.cakeshop.repository;
+
+import br.com.puc.cakeshop.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+}
